e40cd8ab810edb2ef61cc4f8119a22e84d966eb0
Zelalem Alemu <leoxanigm@gmail.com>
2020-04-27 20:24:23 +0200

    init

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //selectors
 
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
 //event listeners
 
+todoButton.addEventListener('click', addTodo);
+
 //functions
+
+function addTodo(e) {
+  e.preventDefault();
+  console.log(e);
+}
